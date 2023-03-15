2743cf72a796811a4496bfbe0930d5f300edce9e
mukundhbalajee <mukundhbalajee520@gmail.com>
2023-03-15 11:12:18 -0400

    Added argument parser and move,state validator

--- StockfishDemo.py
+++ StockfishDemo.py
@@ -3,6 +3,7 @@ module_manager.review()
 from stockfish import Stockfish
 import chess
 import random
+import argparse
 
 class State:
     '''
@@ -11,77 +12,88 @@ class State:
 
     Keeps track of current turn, last move made, current board fen, and game stockfish
     '''
-    def __init__(self):
-        self.turn = 1
+    def __init__(self, fen: str, move: str):
+        # self.turn = 1
         self.stockfish = Stockfish()
-        self.stockfish.set_fen_position(chess.Board().fen())
+        # print(type(chess.Board().fen()))
+        self.stockfish.set_fen_position(fen)
         # TODO add updating to last move made
         self.lastMove = ''
-        self.fen = chess.Board().fen()
+        self.move = move
+        self.fen = fen
 
-    def updateGameState(self, newTurn, newFen, lastMove):
-        self.turn = newTurn
-        self.fen = newFen
-        self.lastMove = lastMove
+def Make_Moves(moves, gameState: State):
+    gameState.stockfish.make_moves_from_current_position(moves)
 
-def Make_Moves(moves, stockfish: Stockfish):
-    stockfish.make_moves_from_current_position(moves)
-
-def Get_Move_From_User(stockfish: Stockfish):
+def Get_Move_From_User(gameState: State):
     '''
     Function to wait for, validate, and apply user input
     :param stockfish:
     :return:
     '''
-    while True:
-        move = input("Enter your move:\n")
-        if stockfish.is_move_correct(move):
-            Make_Moves([move], stockfish)
-            break;
-        else:
-            print("Move is invalid, please try again!")
-    return move
+    move = gameState.move
+    if gameState.stockfish.is_move_correct(move):
+        Make_Moves([move], gameState)
+    else:
+        print("Move is invalid, please try again!")
 
-def AI_Make_Move(stockfish: Stockfish):
+def AI_Make_Move(gameState: State):
     '''
     Function to generate move for AI
     :param stockfish: Stockfish class representing current game being played
     :return: string move -> a string representing move being made by AI
     '''
-    moves = stockfish.get_top_moves(3)
+    moves = gameState.stockfish.get_top_moves(3)
     random.shuffle(moves)
     move = moves[0]['Move']
-    stockfish.make_moves_from_current_position([move])
+    gameState.stockfish.make_moves_from_current_position([move])
     return move
 
-def handleGameState(turn: int, stockfish: Stockfish):
+def handleGameState(gameState: State):
     '''
     Function to handle game state changes
-    :param turn: integer value 0 or 1 representing current player's turn USE FEN NOTATION TO FIGURE OUT TURN
+
+    :param turn: integer value 0 or 1 representing current player's turn 
     :param stockfish: Stockfish class representing current game being played
+
     :return: turn, newFen -> return current player's turn after move is made or not made, updated fen of new board state
     '''
-    if turn:
-        print(stockfish.get_board_visual())      # print current state of the board
-        move = Get_Move_From_User(stockfish)     # request user move
+    turn = gameState.fen.split(" ")[1]
+    if turn == 'w':
+        print(gameState.stockfish.get_board_visual())      # print current state of the board
+        move = Get_Move_From_User(gameState)     # request user move
         turn = 0
     else:
-        move = AI_Make_Move(stockfish)
+        move = AI_Make_Move(gameState)
         print("Opponent made the following move: " + move)
         turn = 1
-    return turn, stockfish.get_fen_position(), move # return new turn and update fen
+    return gameState.stockfish.get_fen_position() # return new turn and update fen
 
-def Start_Game(gameState: State):
+def Validate_State_Move(gameState: State):
     '''
     Start_Game()
     Function used to start a game of chess
     :return:
     '''
-    while (gameState.stockfish.is_fen_valid(gameState.fen)):     # while our board is still valid - no stalemates, no checkmates, etc.
-        newTurn, newFen, lastMove = handleGameState(gameState.turn, gameState.stockfish)
-        gameState.updateGameState(newTurn, newFen, lastMove)
+    if (gameState.stockfish.is_fen_valid(gameState.fen)):     # while our board is still valid - no stalemates, no checkmates, etc.
+        handleGameState(gameState)
+    else:
+        print("Not a valid FEN")
+    print(gameState.stockfish.get_board_visual()) 
     return -1
 
 if __name__ == '__main__':
-    gameState = State()
-    Start_Game(gameState)
+    #TODO: Make the arguments required
+    parser = argparse.ArgumentParser(description='Validate Board State and Move Made')
+    parser.add_argument('-move', metavar='M', type=str, nargs='?',
+                        help='A move made by the user/opponent')
+    parser.add_argument('-fen', metavar='F', type=str, nargs='?',
+                        help='Current FEN state of game')
+
+    args = parser.parse_args()
+    move = args.move
+    fen = args.fen
+
+
+    gameState = State(fen, move)
+    Validate_State_Move(gameState)
